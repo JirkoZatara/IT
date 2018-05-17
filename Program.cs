b14f644412304a5769aa43b7d3f572a0d27584e8
JirkoZatara <38974280+JirkoZatara@users.noreply.github.com>
2018-05-17 17:20:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿/* A Timm-mm-yy!! program in C#
+This program displays the string "Hello Timm-mm-yy!!" on the screen. */
+
+using System;
+namespace HelloTimmy
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello Timm-mm-yy!!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
